c1cd33e1caf078e68c5a5457b9ea5f5a8c371113
Bello Abdulyekini <belloabdulyekini52@gmail.com>
2024-07-28 15:27:44 +0100

    d

--- src/Lecturer.jsx
+++ src/Lecturer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Student = () => {
+const Lecturer = () => {
   return <div></div>;
 };
 
-export default Student;
+export default Lecturer;
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Student = () => {
+const Login = () => {
   return <div></div>;
 };
 
-export default Student;
+export default Login;
