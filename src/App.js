d7c699bbe95300be25c748964ced81deb70468ed
Bello Abdulyekini <belloabdulyekini52@gmail.com>
2024-07-28 15:39:39 +0100

    n

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Lecturer from "./Lecturer";
+import Student from "./Student";
+import Login from "./Login";
 
 function App() {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path="/Home" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
