3944c696087ff40d49eb734ddc87793e2ecafc9c
Bello Abdulyekini <belloabdulyekini52@gmail.com>
2024-07-30 17:14:03 +0100

    me

--- src/Lecturer.jsx
+++ src/Lecturer.jsx
@@ -35,6 +35,12 @@ const Lecturer = () => {
               placeholder="Enter your department here...."
             />
           </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>
+              <b>Create a Password</b>
+            </Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
 
           <Button variant="primary" type="submit">
             Submit
