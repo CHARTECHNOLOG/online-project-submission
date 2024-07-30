f6dea23d4d2108b839f35b1fa8119db365e14125
Bello Abdulyekini <belloabdulyekini52@gmail.com>
2024-07-30 16:36:59 +0100

    lk

--- src/Lecturer.jsx
+++ src/Lecturer.jsx
@@ -9,6 +9,7 @@ const Lecturer = () => {
     <>
       <Container fluid className="Form-Container">
         <Form className="form-content">
+          <h1>Register Has a Lecturer</h1>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
@@ -18,24 +19,25 @@ const Lecturer = () => {
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
+            <Form.Label>Create a Password</Form.Label>
             <Form.Control type="password" placeholder="Password" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Label>Department</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter your department here...."
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Label></Form.Label>
+            <Form.Control type="password" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
+            <Form.Label></Form.Label>
             <Form.Control type="password" placeholder="Password" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
+
           <Button variant="primary" type="submit">
             Submit
           </Button>
