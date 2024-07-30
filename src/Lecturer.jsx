d9d507d876741d5fd2794f61e7ef9355397117fa
Bello Abdulyekini <belloabdulyekini52@gmail.com>
2024-07-30 17:09:50 +0100

    lm

--- src/Lecturer.jsx
+++ src/Lecturer.jsx
@@ -10,31 +10,33 @@ const Lecturer = () => {
       <Container fluid className="Form-Container">
         <Form className="form-content">
           <h1>Register Has a Lecturer</h1>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>
+              <b>Full Name</b>
+            </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Please enter your full name here ....."
+            />
+          </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
+            <Form.Label>
+              <b>Email address</b>
+            </Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Create a Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Department</Form.Label>
+            <Form.Label>
+              <b>Department</b>
+            </Form.Label>
             <Form.Control
               type="text"
               placeholder="Enter your department here...."
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label></Form.Label>
-            <Form.Control type="password" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label></Form.Label>
+            <Form.Label>Create a Password</Form.Label>
             <Form.Control type="password" placeholder="Password" />
           </Form.Group>
 
