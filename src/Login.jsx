98e8dde4008b7eb40da8c494bd4fa377aa5aaebc
Bello Abdulyekini <belloabdulyekini52@gmail.com>
2024-08-09 19:12:19 +0100

    a

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+
 import "../src/Login.css";
 import Container from "react-bootstrap/Container";
 
@@ -8,28 +8,17 @@ const Login = () => {
   return (
     <>
       <Container fluid className="Form-Container">
-        <Form className="form-content">
-          <h1>Login Page</h1>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>
-              <b>Full Name</b>
-            </Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Please enter your full name here ....."
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>
-              <b>Enter your Password</b>
-            </Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-
-          <Button variant="primary" type="submit">
-            Login
+        <div className="heading-text">
+          <h1>Choose which Account you want to login into</h1>
+        </div>
+        <div className="btn-container">
+          <Button variant="primary" type="submit" className="btn-login-1">
+            <a href="/"> Login as a Lecturer</a>
           </Button>
-        </Form>
+          <Button variant="primary" type="submit" className="btn-login-1">
+            <a href="/"> Login as a Student</a>
+          </Button>
+        </div>
       </Container>
     </>
   );
