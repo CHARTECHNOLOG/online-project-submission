bd6d48f17e4e54e62174ff2b52a0d2d0863d21bd
Bello Abdulyekini <belloabdulyekini52@gmail.com>
2024-08-09 19:26:28 +0100

    tt

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-
+import Image2 from "../src/Images/images.jpeg";
 import "../src/Login.css";
 import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/esm/Image";
 
 const Login = () => {
   return (
     <>
       <Container fluid className="Form-Container">
+        <Image src={Image2} className="school-logo" />
         <div className="heading-text">
-          <h1>Choose which Account you want to login into</h1>
+          <h1>Choose which Account do you want to login into</h1>
         </div>
         <div className="btn-container">
           <Button variant="primary" type="submit" className="btn-login-1">
---
+++ src/Studentadmin.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Studentadmin = () => {
+  return <div></div>;
+};
+
+export default Studentadmin;
